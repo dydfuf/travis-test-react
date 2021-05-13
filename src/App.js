4685adae4c58f4871574462e89e34c9c45cddd42
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-05-13 14:17:32 +0900

    cicd test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React App with CI/CD
+          Capstone CI/CD
         </p>
         <a
           className="App-link"
